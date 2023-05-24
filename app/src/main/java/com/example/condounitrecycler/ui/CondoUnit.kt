7e2487a8c98ec2c29b48ff362924579870c9676e
Volnei <Volnei.Foss@townsq.com.br>
2023-05-24 13:46:16 -0300

    alteração na data class CondoUnit

--- app/src/main/java/com/example/condounitrecycler/ui/CondoUnit.kt
+++ app/src/main/java/com/example/condounitrecycler/ui/CondoUnit.kt
@@ -1,37 +1,8 @@
 package com.example.condounitrecycler.ui
 
-import android.annotation.SuppressLint
-import android.os.Parcel
-import android.os.Parcelable
-
-@SuppressLint("ParcelCreator")
 data class CondoUnit(
     val id: String,
     val unitName: String,
     val unitDescription: String,
     val unitPicture: String
-) : Parcelable {
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeString(id)
-        dest.writeString(unitName)
-        dest.writeString(unitDescription)
-        dest.writeString(unitPicture)
-    }
-
-    companion object CREATOR : Parcelable.Creator<CondoUnit> {
-        override fun createFromParcel(parcel: Parcel): CondoUnit {
-            return CondoUnit(parcel.readString().toString(),
-                parcel.readString().toString(),
-                parcel.readString().toString(),
-                parcel.readString().toString())
-        }
-
-        override fun newArray(size: Int): Array<CondoUnit?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+)
